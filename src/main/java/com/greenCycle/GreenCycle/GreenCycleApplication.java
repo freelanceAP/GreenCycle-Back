8e5a9f8c60a99f20c68aee6fe08687d9266328e7
Alexander Pérez <83444111+alexander3130@users.noreply.github.com>
2024-05-12 11:47:22 -0500

    Se crea toda la estructura y las carpetas

---
+++ src/main/java/com/greenCycle/GreenCycle/GreenCycleApplication.java
@@ -0,0 +1,13 @@
+package com.greenCycle.GreenCycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenCycleApplication.class, args);
+	}
+
+}
