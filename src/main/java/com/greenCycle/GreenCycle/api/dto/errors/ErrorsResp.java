577336e2615e4eeb6cd14391fa8d81dcbd83b784
Wendy Walteros <johanawalteros@gmail.com>
2024-05-16 08:55:17 -0500

    Errors y error_handler

--- src/main/java/com/greenCycle/GreenCycle/api/dto/errors/ErrorsResp.java
+++ src/main/java/com/greenCycle/GreenCycle/api/dto/errors/ErrorsResp.java
@@ -1,5 +1,20 @@
 package com.greenCycle.GreenCycle.api.dto.errors;
 
-public class ErrorsResp {
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ErrorsResp extends BaseErrorsResponse {
+    private List<String> errors; 
 
 }
