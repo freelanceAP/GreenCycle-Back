74a24db529ee62ac6cedc331a0585b121faa9111
Wendy Walteros <johanawalteros@gmail.com>
2024-05-19 07:30:54 -0500

    Query
    Query para traer la informacion del mes y las solicitudes que hay en ese mes

--- src/main/java/com/greenCycle/GreenCycle/domain/repositories/RequestRepository.java
+++ src/main/java/com/greenCycle/GreenCycle/domain/repositories/RequestRepository.java
@@ -1,10 +1,23 @@
 package com.greenCycle.GreenCycle.domain.repositories;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.greenCycle.GreenCycle.domain.entities.RequestEntity;
 
 public interface RequestRepository extends JpaRepository<RequestEntity, Long> {
 
-    
+    @Query(value = "SELECT MONTHNAME(r.dateTime) AS month, COUNT(*) AS totalRequests" +
+    "FROM requests r " +
+    "WHERE r.date_time >= :startDate " +
+    "AND r.date_time <= :endDate " +
+    "GROUP BY MONTHNAME(r.dateTime)", nativeQuery = true)
+
+     List<Object[]> findRequestsByDateRange(
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate);
 }
